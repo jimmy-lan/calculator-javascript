32d05448d99da561092b4f2aea91a3c29bab2ad3
Jimmy <jimmylanbcn@gmail.com>
2020-08-02 16:41:20 -0400

    Add javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+// Selectors
+const calculatorDisplay = document.querySelector(".calculator h1");
+const inputButtons = document.querySelectorAll(".calculator-buttons button");
+const btnClear = document.getElementById("btn-clear");
+
+// Event Listeners
+btnClear.addEventListener("click", resetDisplay);
+
+inputButtons.forEach((inputButton) => {
+  if (inputButton.classList.contains("number")) {
+    inputButton.addEventListener("click", () => {
+      sendNumber(inputButton.value);
+    });
+  } else if (inputButton.classList.contains("operator")) {
+    inputButton.addEventListener("click", () => {
+      sendNumber(inputButton.value);
+    });
+  } else if (inputButton.classList.contains("decimal"))
+    inputButton.addEventListener("click", () => {
+      sendNumber(inputButton.value);
+    });
+});
+
+// Helper Functions
+function sendNumber(number) {
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+function resetDisplay() {
+  calculatorDisplay.textContent = "0";
+}
