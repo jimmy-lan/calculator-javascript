cb24c30e733b19c41bde1404fa90540bdc8fd349
Jimmy <jimmylanbcn@gmail.com>
2020-08-03 13:13:47 -0400

    Update functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global variables
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 // Selectors
 const calculatorDisplay = document.querySelector(".calculator h1");
 const inputButtons = document.querySelectorAll(".calculator-buttons button");
@@ -13,24 +18,62 @@ inputButtons.forEach((inputButton) => {
     });
   } else if (inputButton.classList.contains("operator")) {
     inputButton.addEventListener("click", () => {
-      sendNumber(inputButton.value);
+      useOperator(inputButton.value);
     });
   } else if (inputButton.classList.contains("decimal"))
     inputButton.addEventListener("click", addDecimal);
 });
 
+// Calculation object
+const calculate = {
+  "/": (a, b) => a / b,
+  "*": (a, b) => a * b,
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "=": (a, b) => b,
+};
+
 // Helper Functions
 function sendNumber(number) {
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
+function useOperator(operator) {
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operatorValue;
+    return;
+  }
+
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    const result = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = result;
+    firstValue = result;
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
 }
 
 function resetDisplay() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
   calculatorDisplay.textContent = "0";
 }
 
 function addDecimal() {
+  // Do not add decimal point when an operator is pressed
+  if (awaitingNextValue) return;
+
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent += ".";
   }
